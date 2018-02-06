369bfc4c9d149bd35c399997bc94ec866a6e5e0c
skylinelin <linfayang@live.com>
2018-02-06 22:34:34 +0800

    页面Top

---
+++ app/src/main/java/com/skylinelin/deliciousfood/MainActivity.java
@@ -0,0 +1,17 @@
+package com.skylinelin.deliciousfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author skylinelin
+ * @date 2018/2/6
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
